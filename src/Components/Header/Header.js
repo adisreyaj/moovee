00b94287384351bc3b4796302e93167aefd369ff
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-05-10 17:24:30 +0530

    💄 Updated header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,18 +4,21 @@
  * File Created: Friday, 8th May 2020 8:07:40 pm
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Saturday, 9th May 2020 12:38:35 am
+ * Last Modified: Sunday, 10th May 2020 5:22:03 pm
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
 
 import React from 'react';
+import logo from '../../Assets/Images/moovee.svg';
 import './Header.css';
 const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
-        <div className="brand">Moovee</div>
+        <div className="brand">
+          <img src={logo} alt="Moovee" />
+        </div>
         <nav className="menu">
           <ul>
             <li>Home</li>
@@ -25,10 +28,9 @@ const Header = () => {
         </nav>
         <button className="icon-primary mobile-menu-toggle">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-            <path fill="none" d="M0 0h24v24H0z" />
             <path
               d="M16 18v2H5v-2h11zm5-7v2H3v-2h18zm-2-7v2H8V4h11z"
-              fill="rgba(255,245,245,1)"
+              fill="#120d31"
             />
           </svg>
         </button>
