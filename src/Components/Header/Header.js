3f24b8dd34b435a33f54b3dd4fbd4b5ac9c899a9
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-05-10 21:53:17 +0530

    ✨ Add close button for mobile menu

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@
  * File Created: Friday, 8th May 2020 8:07:40 pm
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Sunday, 10th May 2020 9:47:13 pm
+ * Last Modified: Sunday, 10th May 2020 9:52:55 pm
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
@@ -49,12 +49,24 @@ const Header = () => {
           className="icon-primary mobile-menu-toggle"
           onClick={() => toggleMenu(!menuDisplay)}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-            <path
-              d="M16 18v2H5v-2h11zm5-7v2H3v-2h18zm-2-7v2H8V4h11z"
-              fill="#120d31"
-            />
-          </svg>
+          {menuDisplay ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path fill="none" d="M0 0h24v24H0z" />
+              <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z" />
+            </svg>
+          ) : (
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+              <path
+                d="M16 18v2H5v-2h11zm5-7v2H3v-2h18zm-2-7v2H8V4h11z"
+                fill="#120d31"
+              />
+            </svg>
+          )}
         </button>
       </div>
     </header>
