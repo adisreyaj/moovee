b33f93c30faa46a1b3d902b6e7155b31af3094a9
Adithya Sreyaj <adi.sreyaj@gmail.com>
2020-05-17 13:52:01 +0530

    💫 Add menu toggle animation for mobile

--- src/Components/Backdrop/Backdrop.js
+++ src/Components/Backdrop/Backdrop.js
@@ -4,7 +4,7 @@
  * File Created: Friday, 15th May 2020 11:30:08 pm
  * Author: Adithya Sreyaj
  * -----
- * Last Modified: Friday, 15th May 2020 11:33:20 pm
+ * Last Modified: Sunday, 17th May 2020 1:39:24 pm
  * Modified By: Adithya Sreyaj<adi.sreyaj@gmail.com>
  * -----
  */
@@ -12,6 +12,9 @@
 import React from 'react';
 import styles from './Backdrop.module.css';
 
-export default function Backdrop(props) {
-  return props.enabled ? <div className={styles.backdrop}></div> : null;
+export default function Backdrop({ enabled }) {
+  let classes = [styles.backdrop];
+  if (enabled) classes.push(styles.active);
+
+  return <div className={classes.join(' ')}></div>;
 }
